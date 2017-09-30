4891dc5eabb39ee7fbfe42b37b188d8e727ca2d6
yettie79 <lenny.henderson@bigpond.com>
2017-09-30 13:55:36 +1000

    units added

--- addons/160_Populate/config_units.hpp
+++ addons/160_Populate/config_units.hpp
@@ -2,11 +2,38 @@
 class SOR_PopulateUnits {
     class civ {
         name = "C Civilian";
-        units[] = {"C_man_1","C_man_polo_1_F"};
+        units[] = {"C_man_1","C_Man_casual_1_F","C_Man_casual_2_F","C_Man_casual_3_F","C_Man_casual_4_F","C_Man_casual_5_F","C_Man_casual_6_F","C_man_polo_1_F","C_man_polo_2_F","C_man_p_fugitive_F","C_Man_UtilityWorker_01_F","C_Man_Fisherman_01_F","C_Man_ConstructionWorker_01_Black_F"};
     };
-    
     class NATO {
         name = "B NATO";
         units[] = {"B_Soldier_F","B_Soldier_AR_F"};
+    };
+	    class Nazi_ss {
+        name = "O Nazi SS";
+        units[] = {"SS_Trooper_EXP","SS_Trooper_MG","SS_Trooper_RM","SS_Trooper_BR"};
+    };
+    class Nazi_woodland {
+        name = "O Nazi Woodland";
+        units[] = {"Reich_Commander_W","Reich_Grenadier_w","Reich_AT_Rifleman_w","Reich_Heavy_MG_w","Reich_Grenadier_w","Reich_Commander_W","Reich_MM_w","Reich_Gunner_W"};
+    };    
+	class Nazi_Desert {
+        name = "O Nazi Desert";
+        units[] = {"Reich_Commander","Reich_Grenadier","Reich_AT_Rifleman","Reich_Heavy_MG","Reich_Grenadier","Reich_Commander","Reich_MM","Reich_Gunner"};
+    };
+    class CSAT_URBAN {
+        name = "O CSAT URBAN";
+        units[] = {"O_SoldierU_SL_F","O_soldierU_F","O_soldierU_LAT_F","O_soldierU_M_F","O_soldierU_TL_F","O_soldierU_AR_F","O_soldierU_A_F","O_soldierU_medic_F"};
+    };
+	class CSAT_Desert {
+        name = "O CSAT Desert";
+        units[] = {"O_Soldier_SL_F","O_Soldier_F","O_Soldier_LAT_F","O_soldier_M_F","O_Soldier_TL_F","O_Soldier_AR_F","O_Soldier_A_F","O_medic_F"};
+    };
+	class RUS_VDV {
+        name = "O Russia VDV";
+        units[] = {"rhs_msv_sergeant","rhs_msv_efreitor","rhs_msv_grenadier_rpg","rhs_msv_strelok_rpg_assist","rhs_msv_machinegunner","rhs_msv_LAT","rhs_msv_strelok_rpg_assist","rhs_msv_rifleman","rhs_msv_rifleman","rhs_msv_medic"};
+    };
+	class SYND {
+        name = "I Syndikat";
+        units[] = {"I_C_Soldier_Para_6_F","I_C_Soldier_Para_5_F","I_C_Soldier_Para_7_F","I_C_Soldier_Para_8_F","I_C_Soldier_Bandit_4_F","I_C_Soldier_Bandit_3_F","I_C_Soldier_Bandit_5_F","I_C_Soldier_Bandit_1_F"};
     };
 };
